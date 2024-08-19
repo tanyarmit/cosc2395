a64c4b15e591df78bd571511d559adbf42817411
Tanya Unterberger <e102767@rmit.edu.au>
2024-08-19 10:56:46 +1000

    Add exception handling to Q1, Q2, and Q3

---
+++ src/main/java/com/rmit/week6/Q2.java
@@ -0,0 +1,16 @@
+package com.rmit.week6;
+
+public class Q2 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Statement 1");
+            System.out.println("Statement 2" + (10 / 0));
+            System.out.println("Statement 3");
+        } catch (IndexOutOfBoundsException ex) {
+            // } catch (ArithmeticException ex1) {
+            // } catch (RuntimeException ex2) {
+        }
+        System.out.println("Statement 4");
+
+    }
+}
